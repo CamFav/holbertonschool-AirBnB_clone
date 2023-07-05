4fc9275b1d8fca9607f4e023d22ab7c0eded02a9
Meashyshy <camillefvrl@gmail.com>
2023-07-05 12:25:55 -0700

    First try to fix check 13

--- models/base_model.py
+++ models/base_model.py
@@ -23,7 +23,7 @@ class BaseModel:
     def to_dict(self):
         """Return dict representation"""
         dict = self.__dict__.copy()
-        dict['__class__'] = self.__class__.__name__
+        dict['__class__'] = type(self).__name__
         dict['created_at'] = self.created_at.isoformat()
         dict['updated_at'] = self.updated_at.isoformat()
         return dict
