91ab97fe3f5952fa5ad8db4e592e6b888bf2268b
Meashyshy <camillefvrl@gmail.com>
2023-07-05 16:35:39 -0700

    updated with kwargs (use of fromisoformat)

--- models/base_model.py
+++ models/base_model.py
@@ -3,14 +3,23 @@ import uuid
 from datetime import datetime
 """Base Model"""
 
+
 class BaseModel:
     """Base Class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    self.created_at = datetime.fromisoformat(value)
+                    self.updated_at = datetime.fromisoformat(value)
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation"""
@@ -23,7 +32,7 @@ class BaseModel:
     def to_dict(self):
         """Return dict representation"""
         dict = self.__dict__.copy()
-        dict['__class__'] = self.__class__.__name__
+        dict['__class__'] = type(self).__name__
         dict['created_at'] = self.created_at.isoformat()
         dict['updated_at'] = self.updated_at.isoformat()
         return dict
