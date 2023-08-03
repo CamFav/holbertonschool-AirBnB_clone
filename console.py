72edb8f7cec0366510253686bffca6dce5eedee9
Meashyshy <camillefvrl@gmail.com>
2023-08-03 02:14:54 -0700

    Deleted BaseModel

--- console.py
+++ console.py
@@ -4,24 +4,6 @@ import sys
 from datetime import datetime
 import json
 
-class BaseModel:
-    def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-    def __str__(self):
-        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
-
-    def save(self):
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'created_at': self.created_at.isoformat(),
-            'updated_at': self.updated_at.isoformat()
-        }
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
